7527b61a0b1f651adcca32115eebb2a14f266c56
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2016-07-11 23:04:27 +0200

    migrate to test

--- src/test/scala/LensSpec.scala
+++ src/test/scala/LensSpec.scala
@@ -1,8 +1,12 @@
-package fr.xebia.lenses
+import fr.xebia.lenses.Main.{Address, Person}
+import org.scalatest.{FlatSpec, Matchers}
 
-object Main extends App {
+class LensSpec extends FlatSpec with Matchers {
+
   case class Person(name: String, age: Int, address: Address)
+
   case class Address(number: Int, street: String)
+
   trait Lens[O, P] {
     self =>
     def get: O => P
@@ -15,30 +19,38 @@ object Main extends App {
       }
     }
   }
+
   implicit val _personName = new Lens[Person, String] {
     override def get: (Person) => String = _.name
     override def set: (Person, String) => Person = {
       case (p, n) => p.copy(name = n)
     }
   }
+
   implicit val _personAddress = new Lens[Person, Address] {
     override def get: (Person) => Address = _.address
     override def set: (Person, Address) => Person = {
       case (p, a) => p.copy(address = a)
     }
   }
+
   implicit val _addressNumber = new Lens[Address, Int] {
     override def get: (Address) => Int = _.number
     override def set: (Address, Int) => Address = {
       case (a, n) => a.copy(number = n)
     }
   }
-  val person = Person("Toto", 12, Address(10, "street"))
-  val _personAdressNumber: Lens[Person, Int] = _personAddress.and(_addressNumber)
-  val p = _personName.update("Mathieu")(person)
-  val n = _personAdressNumber.get(p)
-  val newP = _personAdressNumber.update(15)(person)
-  println(p)
-  println(n)
-  println(newP)
+
+  "simple lens" should "modify direct value" in {
+    val person = Person("Toto", 12, Address(10, "street"))
+    val _personAddressNumber: Lens[Person, Int] = _personAddress.and(_addressNumber)
+    val p = _personName.update("Mathieu")(person)
+    val n = _personAddressNumber.get(p)
+    val newP = _personAddressNumber.update(15)(person)
+
+    p.toString shouldBe "Person(Mathieu,12,Address(10,street))"
+    n shouldBe 10
+    newP.toString shouldBe "Person(Toto,12,Address(15,street))"
+  }
+
 }
